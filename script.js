fdc0f65bd914b1ce90624ac57fab445c8775ec4b
William Gigliozzi <gigliozzi@gmail.com>
2024-06-25 18:36:10 -0300

    Add require script

---
+++ script.js
@@ -0,0 +1,7 @@
+import Highcharts, { chart } from "highcharts";
+// Load module after Highcharts is loaded
+require("highcharts/modules/exporting")(Highcharts);
+// Create the chart
+chart("container", {
+  /*Highcharts options*/
+});
